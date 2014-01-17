d8f3d78d3d477647a8313a800bc82c2fba647bb5
Chris Alfonso <calfonso@redhat.com>
2014-01-17 12:25:19 -0500

    Making the auth header static.

--- com/redhat/valves/tomcat7/RemoteUserValve.java
+++ com/redhat/valves/tomcat7/RemoteUserValve.java
@@ -13,11 +13,11 @@ import org.apache.catalina.realm.GenericPrincipal;
 
 public class RemoteUserValve extends ValveBase {
     private static final String NOOP = "noop";
-
+    private static final String AUTH_HEADER = "HTTP_AUTH_USER";
     public void invoke(Request request, Response response) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = request.getRequest();
         final List<String> roles = new ArrayList<String>();
-        String authUser = httpServletRequest.getHeader("HTTP_AUTH_USER");
+        String authUser = httpServletRequest.getHeader(AUTH_HEADER);
         final Principal principal = new GenericPrincipal(authUser, NOOP, roles);
         request.setUserPrincipal(principal);
         getNext().invoke(request, response);
